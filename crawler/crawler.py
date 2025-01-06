8dada79c329269059d1a41f4112cbcb368b10955
SOL1archive <enlightkorean@gmail.com>
2025-01-06 22:50:06 +0900

    update:crawler wait time

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -10,3 +10,10 @@ class Crawler:
     def __init__(self, config):
         self.config = config
         self.driver = webdriver.Chrome()
+
+    def wait_randomly(self, min=0.3, max=2):
+        time.sleep(random.uniform(min, max))
+
+    def implicitly_wait(self, implicitly_wait_time=5, min=0.3, max=2):
+        self.driver.implicitly_wait(implicitly_wait_time)
+        self.wait_randomly(min, max)
